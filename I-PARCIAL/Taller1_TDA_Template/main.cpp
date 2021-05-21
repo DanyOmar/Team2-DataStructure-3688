70fcc5b2c04abad4f89cf179b4aa7e125735f6a8
DanyOmar <57681004+DanyOmar@users.noreply.github.com>
2021-05-20 21:16:35 -0500

    Update main.cpp

--- I-PARCIAL/Taller1_TDA_Template/main.cpp
+++ I-PARCIAL/Taller1_TDA_Template/main.cpp
@@ -16,15 +16,15 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	float valor1, valor2;
-	cout<<"Ingrese el valor [1]: "<<endl;
-	cin>>valor1;
-	cout<<"Ingrese el valor [2]: "<<endl;
-	cin>>valor2;
+	cout << "Ingrese el valor [1]: "<<endl;
+	cin >> valor1;
+	cout << "Ingrese el valor [2]: "<<endl;
+	cin >> valor2;
 	Operaciones<float>operacion(valor1,valor2);
-	cout <<"Suma: "<<operacion.suma()<<endl;
-	cout <<"Resta: "<<operacion.resta()<<endl;
-	cout <<"Multiplicación: "<<operacion.multiplicacion()<<endl;
-	cout <<"División: "<<operacion.division()<<endl;
+	cout << "Suma: "<<operacion.suma() << endl;
+	cout << "Resta: "<<operacion.resta() << endl;
+	cout << "Multiplicación: "<<operacion.multiplicacion() << endl;
+	cout << "División: "<<operacion.division() << endl;
 	return 0;
 }
 
