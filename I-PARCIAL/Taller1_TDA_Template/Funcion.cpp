985f90a9c3918e5af9d76f112953b5cef3ec31e2
Johnny Loachamin <57681011+JohnnyLs@users.noreply.github.com>
2021-05-20 21:58:43 -0500

    Update c docs

--- I-PARCIAL/Taller1_TDA_Template/Funcion.cpp
+++ I-PARCIAL/Taller1_TDA_Template/Funcion.cpp
@@ -11,18 +11,47 @@
 
 using namespace std;
 
+////////////////////////////////////////////////////////////////////////
+// Name:       <typename tipoDato>
+// Purpose:    Implementation of <dato>::suma(Operaciones<dato> operaciones)
+//Parameters:   valor1,valor2
+// Return:     template
+////////////////////////////////////////////////////////////////////////
+
 template <typename dato>
 dato Funcion <dato>::suma(Operaciones<dato> operaciones){
 	return operaciones.getValor1()+operaciones.getValor2();
 } 
+////////////////////////////////////////////////////////////////////////
+// Name:       <typename tipoDato>
+// Purpose:    Implementation of <dato>::resta(Operaciones<dato> operaciones)
+//Parameters:   valor1,valor2
+// Return:     template
+////////////////////////////////////////////////////////////////////////
+
 template <typename dato>
 dato Funcion <dato>::resta(Operaciones<dato> operaciones){
 	return operaciones.getValor1()-operaciones.getValor2();
 } 
+
+////////////////////////////////////////////////////////////////////////
+// Name:       <typename tipoDato>
+// Purpose:    Implementation of <dato>::multiplication(Operaciones<dato> operaciones)
+//Parameters:   valor1,valor2
+// Return:     template
+////////////////////////////////////////////////////////////////////////
 template <typename dato>
 dato Funcion <dato>::multiplicacion(Operaciones<dato> operaciones){
 	return operaciones.getValor1()*operaciones.getValor2();
 } 
+
+////////////////////////////////////////////////////////////////////////
+// Name:       <typename tipoDato>
+// Purpose:    Implementation of <dato>::division(Operaciones<dato> operaciones)
+//Parameters:   valor1,valor2
+// Return:     template
+////////////////////////////////////////////////////////////////////////
+
 template <typename dato>
 dato Funcion <dato>::division(Operaciones<dato> operaciones){
 	    if(operaciones.getValor2()==0){
