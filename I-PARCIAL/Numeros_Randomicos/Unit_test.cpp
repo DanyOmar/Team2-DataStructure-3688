5cc0d54e257be7dd0a6029faf155f7fe3bdbbbe3
santychasi <57681109+santychasi@users.noreply.github.com>
2021-05-31 07:03:57 -0500

    Create Unit_test.cpp

---
+++ I-PARCIAL/Numeros_Randomicos/Unit_test.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Deber2/Vector.cpp"
+#include "../Deber2/Operaciones.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			float vec[10] = { 1,2,3,4,5,6,7,8,9,10 };
+			Operaciones<float> operaciones;
+			Vector<float> vector;
+			operaciones.set_vect(tipoDato vec[10]);
+			Assert::AreEqual({ 1,2,3,4,5,6,7,8,9,10 }, funcion.encerar(vec[10]);
+			Assert::AreEqual({ 1,2,3,4,5,6,7,8,9,10 }, funcion.ingresar(vec[10]);
+			Assert::AreEqual({ 1,2,3,4,5,6,7,8,9,10 }, funcion.procesar(vec[10]);
+			Assert::AreEqual({ 1,2,3,4,5,6,7,8,9,10 }, funcion.imprimir(vec[10]);
+		}
+	};
+}
